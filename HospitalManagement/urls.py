6caf1f887f38fca2a7e5759b344231320cf6eeb7
Rishab Raj <rr0948555@gmail.com>
2026-07-07 17:32:05 +0530

    Patient Page

--- HospitalManagement/urls.py
+++ HospitalManagement/urls.py
@@ -25,6 +25,10 @@ urlpatterns = [
     path('', Index,name='home'),
     path('admin_login/', Login,name='login'),
     path('logout/', Logout_admin,name='logout'),
-    path('add_doctor/', Add_Doctor,name='view_doctor'),
+    path('view_doctor/', View_Doctor,name='view_doctor'),
+    path('add_doctor/', Add_Doctor,name='add_doctor'),
     path('delete_doctor(?P<int:pid>)', Delete_Doctor,name='delete_doctor'),
+    path('view_patient/', View_Patient,name='view_patient'),
+    path('add_patient/', Add_Patient,name='add_patient'),
+    path('delete_patient(?P<int:pid>)', Delete_Patient,name='delete_patient'),
 ]
--- hospital/views.py
+++ hospital/views.py
@@ -68,3 +68,38 @@ def Delete_Doctor(request,pid):
     doctor = Doctor.objects.get(id=pid)
     doctor.delete()
     return redirect('view_doctor')
+
+
+
+def View_Patient(request):
+    if not request.user.is_staff:
+        return redirect('login')
+    pat = Patient.objects.all()
+    d = {'pat':pat}
+    return render(request, 'view_patient.html',d)
+
+def Add_Patient(request):
+    error=""
+    if not request.user.is_staff:
+        return redirect('login')
+    if request.method=='POST':
+        n = request.POST['name']
+        g = request.POST['gender']
+        m = request.POST['mobile']
+        a = request.POST['address']
+       
+        try:
+            Doctor.objects.create(name=n,gender=g,mobile=m,address=a)
+            error = "no"
+        except:
+            error = "yes"
+    d = {'error':error}            
+    return render(request,'add_patient.html',d)
+
+def Delete_Patient(request,pid):
+    if not request.user.is_staff:
+        return redirect('login')
+    patient = Patient.objects.get(id=pid)
+    patient.delete()
+    return redirect('view_patient')
+
